e1fbb1cb8e2ec4dac9c6b92b87446a0ae26294c2
MaAs0706e <aswanthmadhav07@gmail.com>
2025-09-09 10:45:12 +0530

    fix : fixed the path error of the login file

---
+++ src/main/java/org/habittracker/utils/Main.java
@@ -0,0 +1,27 @@
+package org.habittracker.utils;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Load login.fxml from resources/habittracker/
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/habittracker/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+
+        // Attach CSS from resources/habittracker/style.css
+        scene.getStylesheets().add(getClass().getResource("/habittracker/style.css").toExternalForm());
+
+        stage.setTitle("Habit Tracker - Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
