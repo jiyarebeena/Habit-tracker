d5ae4b4d601d4a590a315c9ebfbfb8a55a4d0e69
MaAs0706e <aswanthmadhav07@gmail.com>
2025-09-14 23:16:04 +0530

    feat : changed the db password to an env variable , but am still having errors

--- src/main/java/org/habittracker/db/DatabaseConnection.java
+++ src/main/java/org/habittracker/db/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/habit_tracker";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "90373245";
+    private static final String DB_USER = System.getenv("HABIT_DB_USER");
+    private static final String DB_PASSWORD = System.getenv("HABIT_DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
